9d680cefca014b8ea312b281dde9b4e02c73f344
Nicolas Diaz Aragon <nicolas@telescopi.ca>
2012-09-11 22:31:30 -0500

    project setup help files
    a script to install django and instructions detailing how to get djanto
    to work on Aptana

---
+++ ProjectSetup/djangoInstall.sh
@@ -0,0 +1,36 @@
+sudo su;
+echo "adding mysql to system path"
+sudo echo "PATH=$PATH:/usr/local/mysql/bin" >> /etc/profile;
+sudo echo "export PATH" >> /etc/profile;
+PATH=$PATH:/usr/local/mysql/bin;
+export PATH;
+echo "mysql added to system path"
+echo "downloading jpg lib"
+curl -O http://www.ijg.org/files/jpegsrc.v8c.tar.gz; 
+tar -xvzf jpegsrc.v8c.tar.gz; 
+cd jpeg-8c; 
+./configure; 
+make; 
+sudo make install; 
+echo "jpeg lib installed"
+echo "downloading freetype lib"
+cd ..; 
+curl -O http://ftp.igh.cnrs.fr/pub/nongnu/freetype/freetype-2.4.5.tar.gz; 
+tar -xvzf freetype-2.4.5.tar.gz; 
+cd freetype-2.4.5; 
+./configure; 
+make; 
+sudo make install;
+echo "freetype installed"
+echo "installing PIP"
+sudo easy_install pip;
+echo "installing django" 
+sudo pip install django;
+echo "installing PIL"
+sudo pip install PIL;
+echo "installing MySQL-python" 
+sudo pip install MySQL-python;
+echo "adding MySQL-python to path"
+sudo echo "export DLYD_LIBRARY_PATH=/usr/local/mysql/lib" >> /etc/profile;
+sudo ln -s /usr/local/mysql/lib/libmysqlclient.18.dylib /usr/local/lib/libmysqlclient.18.dylib;
+

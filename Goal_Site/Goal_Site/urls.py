98f26a633c4b9d6bfdffb4379b59adc67cd4a42b
Nicolas Diaz Aragon <nicolas@telescopi.ca>
2012-09-18 22:32:43 -0500

    Removed django-social-auth. Need to try a different auth module because django social auth isn't documented well enough

--- Goal_Site/Goal_Site/urls.py
+++ Goal_Site/Goal_Site/urls.py
@@ -10,9 +10,8 @@ urlpatterns = patterns('',
     # url(r'^Goal_Site/', include('Goal_Site.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
-    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
-    url(r'', include('social_auth.urls')),
 )
